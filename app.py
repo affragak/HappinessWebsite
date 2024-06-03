bad04431d1168bd90c4fbf479c2d38b2d46df522
affragak <affragak@gmail.com>
2024-06-03 17:53:58 +0000

    added static pages

--- app.py
+++ app.py
@@ -7,5 +7,22 @@ app = Flask (__name__)
 def hello():
   return render_template("home.html")
 
+@app.route("/planning")
+def planning():
+  return render_template("planning.html")  
+
+@app.route("/flowerd")
+def flowerd():
+  return render_template("flowerd.html")  
+
+@app.route("/about")
+def about():
+  return render_template("about.html")  
+
+@app.route("/contact")
+def contact():
+  return render_template("contact.html")  
+
+
 if __name__ == '__main__': app.run(host='0.0.0.0', debug=True)
   
