0427acab331e5cc89af798f84f67c5aaf2739fc9
affragak <affragak@gmail.com>
2024-06-03 19:39:34 +0000

    added email form

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, flash
+from forms import ContactForm
+from flask_mail import Mail, Message
 
 app = Flask (__name__)
+
+app.secret_key = 'my_secrest_key'
+
+mail = Mail()
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = 'contact@example.com'
+app.config["MAIL_PASSWORD"] = 'your-password'
+mail.init_app(app)
+
 
 @app.route("/")
 def hello():
@@ -19,9 +31,24 @@ def flowerd():
 def about():
   return render_template("about.html")  
 
-@app.route("/contact")
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
-  return render_template("contact.html")  
+  form = ContactForm()
+  if request.method == 'POST':
+    if form.validate() == False:
+      flash('All fields are required.')
+      return render_template('contact.html', form=form)
+    else:
+      msg = Message(form.subject.data, sender='contact@example.com', recipients=['your_email@example.com'])
+      msg.body = """ 
+    From: %s <%s> 
+    %s 
+    """ % (form.name.data, form.email.data, form.message.data)
+      mail.send(msg)
+      return 'Form posted.'
+  elif request.method == 'GET':
+    return render_template('contact.html', form=form)
+    
 
 
 if __name__ == '__main__': app.run(host='0.0.0.0', debug=True)
